5ce6db8a0854c5d0f94e6bce10c104834527d194
Nishant Banjade <nishantbanjade07@gmail.com>
2023-11-11 12:06:40 +0530

    add initial project setup and overview

---
+++ src/models/linear-models/linear-model.c
@@ -0,0 +1,8 @@
+// Linear model overview 
+// Regression
+// Cost functions
+#include <stdio.h>
+int main(){
+    printf("Linear model");
+    return 0;
+}
---
+++ src/models/non-linear-models/non-linear-model.c
@@ -0,0 +1,9 @@
+// Basic project directory and file
+// Might change later
+
+#include <stdio.h>
+
+int main(){
+    printf("write non linear model");
+    return 0;
+}
